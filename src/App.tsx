9cf6743e75e89886a92ac7799c5d612a67e85468
Jatinn517 <jatinnagarwalofficial@gmail.com>
2024-12-28 12:58:22 +0530

    Project completed

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ function App() {
   const [state, setState] = useState<string[]>(['0', '0', '0', '0', '0', '0', '0', '0', '0']);
   const [chance, setChance] = useState<'X' | 'O'>('X');
   const [color, setColor] = useState<boolean[]>([false, false, false, false, false, false, false, false, false]);
+  const [winner, setWinner] = useState<string>("");
+  const [winnerSelected, setWinnerSelected] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(0);
+  let restart = false;
 
   const winningCombinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -13,8 +17,16 @@ function App() {
     [0, 4, 8], [2, 4, 6],
   ];
 
-  function ChangeColor(tile1: number, tile2: number, tile3: number) {
-    console.log(tile1, tile2, tile3);
+  function restartGame() {
+    if (!restart) {
+      restart = true;
+    }
+    else {
+      window.location.reload();
+    }
+  }
+
+  function changeColor(tile1: number, tile2: number, tile3: number) {
     setColor(c => {
       const newColor = c;
       newColor[tile1] = true;
@@ -23,19 +35,17 @@ function App() {
       console.log(newColor);
       return newColor;
     })
+    setWinnerSelected(true);
+    setWinner((state[tile1] === 'X') ? "1st player" : "2nd player");
+    window.addEventListener('click', restartGame);
   }
 
   function handleClick(id: number) {
-    if (state[id] !== '0') {
+    if(winnerSelected){
       return;
     }
-
-
-    for (const [tile1, tile2, tile3] of winningCombinations) {
-      if ((state[tile1] === chance || id === tile1) && (state[tile2] === chance || id === tile2) && (state[tile3] === chance || id === tile3)) {
-        ChangeColor(tile1, tile2, tile3);
-        break;
-      }
+    if (state[id] !== '0') {
+      return;
     }
 
     setState((prev) => {
@@ -44,14 +54,35 @@ function App() {
       return newState;
     });
 
+    setCount(c=>{
+      c = c+1;
+      if(c==9){
+        window.addEventListener('click', restartGame);
+        setWinnerSelected(true);
+        setWinner("No one");
+      }
+      return c;
+    })
+
     setChance(c => {
       return (c == 'X') ? 'O' : 'X';
     })
+
+    for (const [tile1, tile2, tile3] of winningCombinations) {
+      if ((state[tile1] === chance || id === tile1) && (state[tile2] === chance || id === tile2) && (state[tile3] === chance || id === tile3)) {
+        changeColor(tile1, tile2, tile3);
+        break;
+      }
+    }
 
   }
 
   return (
     <div className='flex flex-col justify-center min-h-screen'>
+      <h1 className='text-center text-2xl font-bold mb-10'>TIC-TAC-TOE</h1>
+      {!winnerSelected && <h1 className='text-center text-2xl font-bold mb-10'>{(chance==='X')?"1st player turn": "2nd player turn"}</h1>}
+      {winnerSelected && <h1 className='text-center text-2xl font-bold mb-10'>Winner is {winner}</h1>}
+      {winnerSelected && <h1 className='text-center text-2xl font-bold mb-10'>Click again to restart</h1>}
       <div className='grid grid-cols-5 text-center'>
         <div className='flex col-start-2 flex-col justify-normal border-r-2 border-black'>
           <div className={`flex items-center justify-center col-span-1 border-b-2 border-black h-20  ${(color[0] == true) ? "bg-red-500" : ""}`} onClick={() => handleClick(0)}>{(state[0] == '0') ? "" : state[0]}</div>
